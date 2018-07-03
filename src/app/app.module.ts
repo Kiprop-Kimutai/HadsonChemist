951daf9a2a890305d5b5b5bb48b6573872b44a4c
jonah <jonah.kimutai@compulynx.com>
2018-07-03 19:09:08 +0300

    modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,30 +7,38 @@ import {CommonModule} from '@angular/common';
 import { AppComponent } from './app.component';
 import {LoginComponent} from './login/login.component';
 import {LayoutComponent} from './layout/layout.component';
+import {NavListComponent} from './layout/nav-list.component';
 import {PageDirective} from './common/PageDirective';
 import {UsersPageComponent} from './users/users-page.component';
 import {UserFormService} from './users/user-forms.service';
 import {LoginFormComponent} from './login/login-form.component';
 import {RegisterFormComponent} from './login/register-form.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {AuthService} from './login/auth.service';
+import {MenuService} from './layout/menu-service';
+import {MaterialModule} from './material.module';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import { ChartsModule } from 'ng2-charts';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     UsersPageComponent,
     LayoutComponent,
+    NavListComponent,
     LoginFormComponent,
     RegisterFormComponent,
+    DashboardComponent,
     PageDirective
   ],
   imports: [
-    BrowserModule,CommonModule,BrowserAnimationsModule,AppRoutingModule,ReactiveFormsModule,MatCardModule,
-    MatInputModule,MatButtonModule
+    BrowserModule,CommonModule,BrowserAnimationsModule,AppRoutingModule,ReactiveFormsModule,MaterialModule,
+    FlexLayoutModule,ChartsModule
+
   ],
-  providers: [UserFormService],
+  providers: [UserFormService,AuthService,MenuService],
   entryComponents:[LoginFormComponent,RegisterFormComponent],
+  exports:[FlexLayoutModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
