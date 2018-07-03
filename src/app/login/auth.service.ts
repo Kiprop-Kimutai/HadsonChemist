22145fee9f1ebaee789809c28ee3633afaa35d54
jonah <jonah.kimutai@compulynx.com>
2018-07-03 15:33:52 +0300

    Authentication service

---
+++ src/app/login/auth.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {Observable,of} from 'rxjs';
+import {delay,tap} from 'rxjs/operators';
+
+@Injectable()
+export class AuthService{
+  isLoggedIn:boolean = false;
+  loggedInuser:string = '';
+    login(username:string,password:string):Observable<boolean>{
+      console.log("Trying to login....");
+      this.loggedInuser = username;
+      //return Observable.of(true).pipe(delay(1000),tap(val=>isLoggedIn=true));
+      return of(true).pipe(delay(1000),tap(() =>this.isLoggedIn =true));
+    }
+
+    logout():void{
+      this.isLoggedIn = false;
+    }
+
+}
