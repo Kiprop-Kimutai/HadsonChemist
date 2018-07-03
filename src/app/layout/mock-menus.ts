525fb094896fbe6b70bf8b3f855230c97cbcfd66
jonah <jonah.kimutai@compulynx.com>
2018-07-03 15:21:46 +0300

    mock menus

---
+++ src/app/layout/mock-menus.ts
@@ -0,0 +1,10 @@
+import {Menu} from '../models/menus';
+import {SubMenus} from '../models/submenus';
+export const MENUS = [
+      new Menu('dashboard','Dashboard','dashboard','link'),
+      new Menu('users','Users','users','link')
+      new Menu('devices',"Devices",'devices','link'),
+      new Menu('firmware','Firmware','test','parent',[new SubMenus('saral','Saral Info','link'),new SubMenus('newpos','NewPos 9220','link')])
+      new Menu('clients','Clients','client','parent',[new SubMenus('wfp','WFP','link'),new SubMenus('pbu','Postbank UG','link'),new SubMenus('tbc','TBC','link')])
+
+]
