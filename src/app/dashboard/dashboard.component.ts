8cbd55cfad6b93562591ae0137e3e559e991007e
jonah <jonah.kimutai@compulynx.com>
2018-07-09 14:05:36 +0300

    updated dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -8,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
 
   constructor() { }
-
+  showBarChart_gt_sm:boolean;
+  showBarChart_xs:boolean;
+  height:number = 15;
   public lineChartData:Array<any> = [
     {data: [1.1, 1.2, 1.25, 2.00, 2.50, 2.50, 2.50], label: 'SARAL INFO'},
     {data: [1.10, 1.10, 1.20, 1.25, 1.25, 2.00, 2.10], label: 'NEWPOS 9220'}
@@ -62,7 +64,29 @@ public lineChartBarData:Array<any> = [
     console.log(e);
     this.lineChartBarType = this.lineChartBarType === 'line' ? 'bar' :'line';
   }
+  chartHovered(e:any){
+    console.log(e);
+  }
+  getScreenSize():number{
+    console.log("**");
+    console.log(window.innerWidth);
+    return window.innerWidth;
+  }
+  evaluateChartsToRender(x:number):void{
+    console.log("checking init..");
+    let toggleScreen:number = x<400 ? 0:1
+    switch(toggleScreen){
+      case (0):
+        this.showBarChart_xs = true;
+        this.showBarChart_gt_sm = false;
+        break;
+      case (1):
+        this.showBarChart_xs = false;
+        this.showBarChart_gt_sm = true;
+    }
+  }
   ngOnInit() {
+      this.evaluateChartsToRender(this.getScreenSize());
   }
 
 }
