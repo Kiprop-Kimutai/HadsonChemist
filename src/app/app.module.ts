741542eb16a1fd6486ac730e0ed53ed980fd3740
jonah <jonah.kimutai@compulynx.com>
2018-07-09 13:52:50 +0300

    #core,router,rxjs,common and material dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {AppRoutingModule} from './app.routing.module';
-import {ReactiveFormsModule} from '@angular/forms';
+import {ReactiveFormsModule,FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import { AppComponent } from './app.component';
 import {LoginComponent} from './login/login.component';
@@ -14,11 +14,16 @@ import {UserFormService} from './users/user-forms.service';
 import {LoginFormComponent} from './login/login-form.component';
 import {RegisterFormComponent} from './login/register-form.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {AuthService} from './login/auth.service';
-import {MenuService} from './layout/menu-service';
+import {PageNotFoundComponent} from './page-not-found.component';
+import {DeviceListComponent} from './device/device-list.component';
+import {DeviceComponent} from './device/device.component';
+import {FilterDevicess} from './device/filterDevices';
+import {DeviceDialog} from './device/device-dialog';
+import {ServiceModule} from './services.module';
 import {MaterialModule} from './material.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { ChartsModule } from 'ng2-charts';
+import {DialogService} from './dialog.service';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,15 +34,15 @@ import { ChartsModule } from 'ng2-charts';
     LoginFormComponent,
     RegisterFormComponent,
     DashboardComponent,
-    PageDirective
+    PageDirective,DeviceDialog,DeviceComponent,DeviceListComponent,FilterDevicess,PageNotFoundComponent
   ],
   imports: [
-    BrowserModule,CommonModule,BrowserAnimationsModule,AppRoutingModule,ReactiveFormsModule,MaterialModule,
-    FlexLayoutModule,ChartsModule
+    BrowserModule,CommonModule,BrowserAnimationsModule,AppRoutingModule,ReactiveFormsModule,FormsModule,MaterialModule,
+    FlexLayoutModule,ChartsModule,ServiceModule
 
   ],
-  providers: [UserFormService,AuthService,MenuService],
-  entryComponents:[LoginFormComponent,RegisterFormComponent],
+  providers: [UserFormService,DialogService],
+  entryComponents:[LoginFormComponent,RegisterFormComponent,DeviceDialog],
   exports:[FlexLayoutModule],
   bootstrap: [AppComponent]
 })
