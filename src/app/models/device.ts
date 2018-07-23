98e9f398d8a1637fffd240eb3e5de44a742984f2
jonah <jonah.kimutai@compulynx.com>
2018-07-23 10:47:54 +0300

    #modified device model to accomodate previous firmware version

--- src/app/models/device.ts
+++ src/app/models/device.ts
@@ -1,5 +1,5 @@
 export class Device{
-  constructor(public id:number,public macaddress:string,public serialno:string,public model:string,public kernel:string,public firmware:string,public active:boolean){
+  constructor(public id:number,public macaddress:string,public serialno:string,public model:string,public kernel:string,public firmware:string,public assigned_firmware:string,public active:boolean){
 
   }
 }
