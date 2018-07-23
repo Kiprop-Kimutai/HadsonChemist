a2a2599dfc6d54e6b8f0e3504b85dd5ca85c86cb
jonah <jonah.kimutai@compulynx.com>
2018-07-23 10:49:06 +0300

    #provider for app's service modules

--- src/app/services.module.ts
+++ src/app/services.module.ts
@@ -2,7 +2,9 @@ import {NgModule} from '@angular/core';
 import {AuthService} from './login/auth.service';
 import {MenuService} from './layout/menu-service';
 import {DeviceService} from './device/device.service';
+import {AuthGuardService} from './auth-guard.service';
+import {CanDeactivateGuard} from './can-deactivate-guard.service';
 @NgModule({
-  providers:[AuthService,MenuService,DeviceService]
+  providers:[AuthService,MenuService,DeviceService,AuthGuardService,CanDeactivateGuard,CanDeactivateGuard]
 })
 export class ServiceModule{}
