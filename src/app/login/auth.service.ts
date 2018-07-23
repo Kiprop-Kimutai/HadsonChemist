2751af3878e0cab17baba2f9f9d5c6816ab1b47f
jonah <jonah.kimutai@compulynx.com>
2018-07-23 10:43:57 +0300

    #added authentication service to manage sessions and access levels

--- src/app/login/auth.service.ts
+++ src/app/login/auth.service.ts
@@ -6,11 +6,12 @@ import {delay,tap} from 'rxjs/operators';
 export class AuthService{
   isLoggedIn:boolean = false;
   loggedInuser:string = '';
+  redirectUrl:string = "";
     login(username:string,password:string):Observable<boolean>{
       console.log("Trying to login....");
       this.loggedInuser = username;
       //return Observable.of(true).pipe(delay(1000),tap(val=>isLoggedIn=true));
-      return of(true).pipe(delay(1000),tap(() =>this.isLoggedIn =true));
+      return of(true).pipe(delay(100),tap(() => this.isLoggedIn =true));
     }
 
     logout():void{
