3e9876a3498b17a35d850153bd7964b054d56d8f
jonah <jonah.kimutai@compulynx.com>
2018-07-23 10:28:57 +0300

    modified submenus titles to fit routing configuration

--- src/app/layout/mock-menus.ts
+++ src/app/layout/mock-menus.ts
@@ -4,7 +4,7 @@ export const MENUS = [
       new Menu('dashboard','Dashboard','dashboard','link'),
       new Menu('users','Users','users','link'),
       new Menu('devices',"Devices",'devices','link'),
-      new Menu('firmware','Firmware','test','parent',[new SubMenus('saral','Saral Info','link'),new SubMenus('newpos','NewPos 9220','link')]),
+      new Menu('firmware','Firmware','test','parent',[new SubMenus('saral-firmware','Saral Info','link'),new SubMenus('newpos-firmware','NewPos 9220','link')]),
       new Menu('clients','Clients','client','parent',[new SubMenus('wfp','WFP','link'),new SubMenus('pbu','Postbank UG','link'),new SubMenus('tbc','TBC','link')])
 
 ]
